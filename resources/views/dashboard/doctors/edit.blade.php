39690d0e104a6f486a9bd47325b63c39ccffd0e9
Mohamed Salah <43927801+MohamedSalah17@users.noreply.github.com>
2020-11-09 13:34:43 +0200

    update edit doctor activation

--- resources/views/dashboard/doctors/edit.blade.php
+++ resources/views/dashboard/doctors/edit.blade.php
@@ -50,12 +50,23 @@
                             </div>
 
 
-                            <div class="form-group">
+                            {{-- <div class="form-group">
                                 <label>@lang('site.active')*</label>
                                 <select name="active" class="form-control">
                                     <option value="1" {{$doctor->active == 1? 'selected': '' }}>@lang('site.is_active')</option>
                                     <option value="0" {{$doctor->active == 0? 'selected': '' }}>@lang('site.not_active')</option>
                                 </select>
+                            </div> --}}
+
+                            <input type="hidden" name="active" value="0">
+
+                            <div class="form-group">
+                                <label>@lang('site.active')*</label>
+                                <div class="custom-control custom-switch material-switch">
+                                        <input type="checkbox" name="active" class="custom-control-input" id="doctorSwitch{{$doctor->id}}"
+                                        onchange="this.checked? this.value = 1 : this.value = 0" {{ $doctor->active == 1? 'checked' : ''}} >
+                                        <label class="custom-control-label" for="doctorSwitch{{$doctor->id}}"></label>
+                                </div>
                             </div>
 
 
