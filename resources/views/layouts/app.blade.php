19242d09e5570ae5a646ee88d4e3068cfd23cf52
Mohamed Salah <43927801+MohamedSalah17@users.noreply.github.com>
2020-11-11 01:49:08 +0200

    update the login title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- <title>{{ config('app.name', @lang('site.msa')) }}</title> --}}
-<title>{{ trans('المعهد العالي للعلوم الإدارية ببني سويف') }}</title>
+<title>{{ trans('كلية الحاسبات والذكاء الإصطناعي') }}</title>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
 
