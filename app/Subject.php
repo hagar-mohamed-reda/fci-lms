2198f6045cb46a2545b883029f95a8427fa71005
Mohamed Salah <43927801+MohamedSalah17@users.noreply.github.com>
2020-11-28 16:49:40 +0200

    add level_id to courses

--- app/Subject.php
+++ app/Subject.php
@@ -6,17 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subject extends Model
 {
-<<<<<<< HEAD
-    protected $table = "courses";
-    
-=======
     protected $table = "lms_courses";
 
->>>>>>> 26b744230e46ef5c3d71b728612bbb0fc8411cc3
     protected $guarded = [];
 
     protected $fillable = [
-        'name', 'code','doc_id','description','hours','notes'
+        'name', 'code','doc_id','description','hours','notes','level_id'
     ];
 
     public function doctor(){
