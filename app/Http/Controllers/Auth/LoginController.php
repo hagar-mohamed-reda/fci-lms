026fc56bb5a1979cc1224393e6896ea7d22aa259
Mohamed Salah <43927801+MohamedSalah17@users.noreply.github.com>
2020-12-07 15:38:53 +0200

    update login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -101,12 +101,15 @@ class LoginController extends Controller
 
     protected function credentials(Request $request)
         {
-          if(is_numeric($request->get('email'))){
-            return ['phone'=>$request->get('email'),'password'=>$request->get('password'), 'active' =>1];
-          }
-          elseif (filter_var($request->get('email'), FILTER_VALIDATE_EMAIL)) {
+            if(is_numeric($request->get('email'))){
+            return ['username' => $request->get('email'), 'password'=>$request->get('password'), 'active' =>1];
+            }
+            elseif (filter_var($request->get('email'), FILTER_VALIDATE_EMAIL)) {
             return ['email' => $request->get('email'), 'password'=>$request->get('password'), 'active' =>1];
-          }
-          return ['username' => $request->get('email'), 'password'=>$request->get('password'), 'active' =>1];
+            }
+            elseif(is_numeric($request->get('email'))){
+            return ['phone'=>$request->get('email'),'password'=>$request->get('password'), 'active' =>1];
+            }
+            return ['username' => $request->get('email'), 'password'=>$request->get('password'), 'active' =>1];
         }
 }
